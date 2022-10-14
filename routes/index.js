984d35316b01d93972bdcd5ee61cd17bb05ac9f2
Volodymyr <vthehang@gmail.com>
2022-10-14 18:24:39 +0200

    added stories in db, began to work with dashboard, added error pages

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 import { Router } from "express";
 import { ensureAuth, ensureGuest } from "../middleware/auth.js";
+import Story from "../models/Story.js";
+
 const router = Router();
 
 // @desc    Login/Landing page
@@ -12,10 +14,17 @@ router.get("/", ensureGuest, (req, res) => {
 
 // @desc    Dashboard
 // @route   GET /dashboard
-router.get("/dashboard", ensureAuth, (req, res) => {
-    res.render("dashboard", {
-        name: req.user.firstName,
-    });
+router.get("/dashboard", ensureAuth, async (req, res) => {
+    try {
+        const stories = await Story.find({ user: req.user._id }).lean();
+        res.render("dashboard", {
+            name: req.user.displayName,
+            stories,
+        });
+    } catch (e) {
+        console.error(e);
+        res.render("error/500");
+    }
 });
 
 export default router;
